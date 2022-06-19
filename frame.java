7d855bcb5d138784675335cbf5d900b94b32eae8
I10G <105935500+I10G@users.noreply.github.com>
2022-06-19 11:06:01 -0400

    Update frame.java

--- frame.java
+++ frame.java
@@ -5,13 +5,23 @@ public class frame extends JFrame {
     public frame()
     {
         super("Neural Listener");
-        
+
         panel pane = new panel();
+        
+        add(createImage("computer.png", 100, 100, 200, 200));
+        
         add(pane);
         
         setSize(350, 475);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private JLabel createImage(String fileName, int x, int y, int w, int h) {
+        JLabel label = new JLabel();
+        label.setIcon(new ImageIcon(fileName));
+        label.setBounds(x, y, w, h);
+        return label;
     }
 }
